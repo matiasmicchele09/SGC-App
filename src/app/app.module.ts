f6f3171ee7ee343bd2daac0ba22b0f06fc60e01d
Matias Micchele <matias.micchele@hotmail.com>
2025-01-29 16:57:47 -0300

    Creando seccion de autorizacion y dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomersComponent } from './dashboard/pages/customers/customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    DashboardComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule
