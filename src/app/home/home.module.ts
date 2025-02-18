86e018884acb497e40e38ca7d89b965aba4c7ae7
Matias Micchele <matias.micchele@hotmail.com>
2025-02-18 19:40:03 -0300

    Agregando carpeta shared

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CustomersComponent } from './pages/customers/customers.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     CustomersComponent,
     DashboardComponent,
+    SidebarComponent,
   ],
   imports: [
     CommonModule,
